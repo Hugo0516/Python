82848b292ee9c4e5c722c4791c9aef23212711ef
Hugo <hugo051697@gmail.com>
2020-08-11 13:03:13 +0800

    Add Leetcode 92
    First upload version
    
    Just create it, still not finish

---
+++ _92_linkedlist_reverse_linked_list_2.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+
+
+if __name__ == '__main__':
+    demo = Solution()
+   
