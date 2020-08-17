a36dd8677aadc3edc2b879429ff3a8eab8ef6561
Hugo <hugo051697@gmail.com>
2020-08-17 21:42:26 +0800

    leetcode 287 minor add

--- _287_twopointers_find_the_duplicate_number.py
+++ _287_twopointers_find_the_duplicate_number.py
@@ -32,6 +32,11 @@ class Solution:
 
 
 """
+    1. You must not modify the array (assume the array is read only).
+    2. You must use only constant, O(1) extra space.
+    3. Your runtime complexity should be less than O(n2).
+    4. There is only one duplicate number in the array, but it could be repeated more than once.
+    
     第二個方法比較好
     幹你老師 都好難想＝＝ 雞掰
     
