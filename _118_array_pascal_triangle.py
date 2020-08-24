3c939537520a5d2bd06bb3f45f28b5cbea9895f8
Hugo <hugo051697@gmail.com>
2020-08-24 18:26:24 +0800

    _118_array_pascal_triangle.py： FINISHED

---
+++ _118_array_pascal_triangle.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        for i in range(numRows):
+            res.append([])
+            for j in range(i + 1):
+                if j in (0, i):  # 表示j == 0 or j == i 的時候(即頭跟尾)
+                    res[i].append(1)
+                else:
+                    res[i].append(res[i - 1][j - 1] + res[i - 1][j])
+
+        return res
+"""
+https://www.youtube.com/watch?v=gq4t3cwMQbs
+"""
+
+if __name__ == '__main__':
+    demo = Solution()
+    res = demo.generate(5)
+    print(res)
