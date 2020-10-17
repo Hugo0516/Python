257c3ef054590a5ae05e51684f87c043c84a0d1b
Hugo <hugo051697@gmail.com>
2020-10-17 23:32:18 +0800

    _8_string_string_to_integer.py: Fix bugs

--- _8_string_string_to_integer.py
+++ _8_string_string_to_integer.py
@@ -31,15 +31,18 @@ class Solution:
     def myAtoi(self, str: str) -> int:
         intMax = 2147483647
         intMin = -2147483648
-        str = str.strip()
+        str = str.strip()  # remove front space and rear space, if strip('0')=> remove front 0 and rear 0
+
         if not str:
             return 0
+
         sign, i = 1, 0
         if str[i] == "+":
             i += 1
         elif str[i] == "-":
             sign = -1
             i += 1
+
         num = 0
         while i < len(str):
             if not str[i].isdigit():
@@ -48,7 +51,40 @@ class Solution:
             if num > intMax:
                 break
             i += 1
+
         return min(max(sign * num, intMin), intMax)
+
+    # Write by myself
+    def myAtoi2(self, s: str) -> int:
+        int_max = 2147483647
+        int_min = -2147483648
+        s1 = s.strip()
+        start, sign = 0, 1
+        ans = 0
+
+        if not s1:
+            return 0
+
+        if s1[0] is '+':
+            start += 1
+        elif s1[0] is '-':
+            sign = -1
+            start += 1
+
+        for i in range(start, len(s1)):
+            if not s1[i].isdigit():
+                break
+            if s1[i].isdigit():
+                ans = ans * 10 + ord(s1[i]) - ord('0')
+
+        ans *= sign
+        if ans > int_max:
+            return int_max
+        elif ans < int_min:
+            return int_min
+
+        return ans
+
 
 """
     解題思路：
@@ -60,8 +96,13 @@ class Solution:
             https://www.youtube.com/watch?v=vvua0G0eqsM          
 """
 
-
 if __name__ == '__main__':
     demo = Solution()
     input_1 = '   -443212'
+    input_2 = "-91283472332"
+    input_3 = "words and 987"
+    input_4 = "4193 with words"
     print(demo.myAtoi(input_1))
+    print(demo.myAtoi(input_2))
+    print(demo.myAtoi(input_3))
+    print(demo.myAtoi(input_4))
