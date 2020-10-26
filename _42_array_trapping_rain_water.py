829f05b9a88667771e0dc8ce685e506958677438
Hugo <hugo051697@gmail.com>
2020-10-26 22:42:52 +0800

    _42_array_trapping_rain_water.py: Add new methods, and detail

--- _42_array_trapping_rain_water.py
+++ _42_array_trapping_rain_water.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 class Solution:
     # def trap(self, height: List[int]) -> int:
     #     left = 0
@@ -34,25 +31,43 @@ class Solution:
         left, right = 0, len(height) - 1
 
         while left < right:
-            if height[left] < height[right]:  # 左指针操作
+            if height[left] < height[right]:  # 左指針操作
                 if height[left] < left_max:
                     res += left_max - height[left]
                 else:
                     left_max = height[left]
-                left += 1  # 移动左指针
+                left += 1  # 移動左指針
             else:
-                if height[right] < right_max:  # 右指针操作
+                if height[right] < right_max:  # 右指針操作
                     res += right_max - height[right]
                 else:
                     right_max = height[right]
-                right -= 1  # 移动右指针
+                right -= 1  # 移動右指針
         return res
+
+    def trap3(self, height):
+        n = len(height)
+        if n == 0:
+            return None
+        l, r = 0, n - 1
+        max_l, max_r = height[l], height[r]
+        ans = 0
+        while l < r:
+            if max_l < max_r:
+                ans += (max_l - height[l])
+                l += 1
+                max_l = max(max_l, height(l))
+            else:
+                ans += (max_r - height[r])
+                r -= 1
+                max_r = max(max_r, height[r])
+        return ans
 
 
 """
     方法二，使用雙指針法，在左右兩端分別設置指針left, right，並且用left_max， right_max記錄走過之後的最長最高的邊。
     例如， left_max就是指針left左邊最長的線，right_max同理。
-    （1）如果  height[left] < height[right]，就是左指針指向的數字<右指針指向的數字（現在操作左指針）,
+    （1）如果 height[left] < height[right]，就是左指針指向的數字<右指針指向的數字（現在操作左指針）,
     說明此時，左邊已經走過的位置都小於height[right]，通俗的講，現在left指針位置的左右兩邊桶的短板，一定存在於左邊，
     又因為left_max記錄了當前left位置的左邊最高的那個邊，所以當前left位置的容量就是只需考慮left_max ， height[ left]這個兩個值的大小即可得出。
 
@@ -61,9 +76,15 @@ class Solution:
     （2）如果height[left] >= height[right]，同理（1）。直到兩個指針相遇。
     
     https://blog.csdn.net/XX_123_1_RJ/java/article/details/81048041
+    
+    這題有 DP 解法 注意一下
+    
+    trap3 => HuaHua solution
 """
 
 if __name__ == '__main__':
     input_1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
     demo = Solution()
-    print(demo.trap(input_1))
+    # print(demo.trap(input_1))
+
+    print(demo.trap3(input_1))
